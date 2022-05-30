abeced456b158bd6de9c05f24975c007aad22c48
elder-tome <r14.elderalves@gmail.com>
2022-05-30 20:09:54 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className=''>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main className=''>
+        <h1>teste</h1>
+      </main>
+      <footer className=''>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home
